22575ec64da2e261495e646aa057e1e1741c803c
Max Donchenko <wereya2@gmail.com>
2016-05-31 21:31:56 +0300

    Lab 3: added GET requests processing and Examples

--- lab3/server.js
+++ lab3/server.js
@@ -1,7 +1,44 @@
-var app   = require('express')();
+var app = require('express')();
 var http = require('http').Server(app);
 var mysql = require('mysql');
 var bodyParser = require("body-parser");
 http.listen(8080,function(){
+var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : 'root',
+		database : 'hotels',
+	});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // or maybe you prefer XML? ;)
+
+var defaultRequest = function(req, res){
+	var data = {
+		"Data":""
+	};
+	data["Data"] = "REST API: Hotels";
+	res.json(data);
+};	
+app.get('/', defaultRequest);
+app.get('/restapi', defaultRequest);
+
+app.get('/restapi/country', function(req, res) {
+	var data = {
+		"error":1,
+		"Hotels":""
+	};
+	
+	connection.query("SELECT * from Hotel", function(err, rows, fields) {
+		if (rows.length != 0) {
+			data["error"] = 0;
+			data["Hotels"] = rows;
+			res.json(data);
+		} else {
+			data["Hotels"] = 'No hotels found..';
+			res.json(data);
+		}
+	});
+});
+
 	console.log("Connected & Listen to port 8080");
 });
